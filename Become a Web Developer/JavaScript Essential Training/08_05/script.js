595493ee8eac96546aa1a1dcc6b5746b15abfa41
tu-nguyen <valcrune@gmail.com>
2019-07-22 21:52:46 -0400

    progress with js essentials ch8 project

--- Become a Web Developer/JavaScript Essential Training/08_05/script.js
+++ Become a Web Developer/JavaScript Essential Training/08_05/script.js
@@ -4,19 +4,24 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
-var timer = [0,0,0,0];
+var timer = [0, 0, 0, 0];
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
-
+function leadingZero(time) {
+    if (time <= 9) {
+        time = "0" + time;
+    }
+    return time;
+}
 
 // Run a standard minute/second/hundredths timer:
 function runTimer() {
-    let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2];
+    let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
     theTimer.innerHTML = currentTime;
     timer[3]++;
 
-    timer[0] = Math.floor((timer[3]/100)/60);
-    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
+    timer[0] = Math.floor((timer[3] / 100) / 60);
+    timer[1] = Math.floor((timer[3] / 100) - (timer[0] * 60));
     timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
 }
 
--- Become a Web Developer/JavaScript Essential Training/08_06/script.js
+++ Become a Web Developer/JavaScript Essential Training/08_06/script.js
@@ -4,7 +4,7 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
-var timer = [0,0,0,0];
+var timer = [0, 0, 0, 0];
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 function leadingZero(time) {
@@ -20,16 +20,27 @@ function runTimer() {
     theTimer.innerHTML = currentTime;
     timer[3]++;
 
-    timer[0] = Math.floor((timer[3]/100)/60);
-    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
+    timer[0] = Math.floor((timer[3] / 100) / 60);
+    timer[1] = Math.floor((timer[3] / 100) - (timer[0] * 60));
     timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
 }
 
 // Match the text entered with the provided text on the page:
 function spellCheck() {
     let textEntered = testArea.value;
+    let originTextMatch = originText.substring(0, textEntered.length);
 
-    console.log(textEntered);
+    if (textEntered == originText) {
+        testWrapper.style.borderColor = "#429890";
+    } else {
+        if (textEntered == originTextMatch) {
+            testWrapper.style.borderColor = "#65CCF3";
+        } else {
+            testWrapper.style.borderColor = "#E95D0F";
+        }
+    }
+    // console.log(originTextMatch);
+    // console.log(textEntered);
 }
 
 // Start the timer:
