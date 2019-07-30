74a8e015399fd920e907ed71a42b51225a8ed11e
tu-nguyen <valcrune@gmail.com>
2019-07-30 18:48:08 -0400

    finished ch10 project of js essentials

--- Become a Web Developer/JavaScript Essential Training/10_05/JS/script.js
+++ Become a Web Developer/JavaScript Essential Training/10_05/JS/script.js
@@ -1,23 +1,30 @@
 const IMAGES = document.querySelectorAll("img");
+const SIZES = {
+    showcase: "100vw",
+    reason: "(max-width: 799px) 100 vw, 372px",
+    feature: "(max-width: 799px) 100 vw, 558px",
+    story: "(max-width: 799px) 100 vw, 670px"
+}
 
 function makeSrcset(imgSrc) {
     let markup = [];
     let width = 400;
 
-    for (let i = 0; i<5; i++) {
+    for (let i = 0; i < 5; i++) {
         markup[i] = imgSrc + "-" + width + ".jpg " + width + "w";
-        width+=400;
+        width += 400;
     }
 
     return markup.join();
 }
 
-for (let i = 0; i<IMAGES.length; i++) {
+for (let i = 0; i < IMAGES.length; i++) {
     let imgSrc = IMAGES[i].getAttribute("src");
-    imgSrc = imgSrc.slice(0,-8);
+    imgSrc = imgSrc.slice(0, -8);
     let srcset = makeSrcset(imgSrc);
     console.log(srcset);
 
     let type = IMAGES[i].getAttribute("data-type");
-    console.log(type);
+    let sizes = SIZES[type];
+    console.log(sizes);
 }
--- Become a Web Developer/JavaScript Essential Training/10_06/JS/script.js
+++ Become a Web Developer/JavaScript Essential Training/10_06/JS/script.js
@@ -10,21 +10,23 @@ function makeSrcset(imgSrc) {
     let markup = [];
     let width = 400;
 
-    for (let i = 0; i<5; i++) {
+    for (let i = 0; i < 5; i++) {
         markup[i] = imgSrc + "-" + width + ".jpg " + width + "w";
-        width+=400;
+        width += 400;
     }
 
     return markup.join();
 }
 
-for (let i = 0; i<IMAGES.length; i++) {
+for (let i = 0; i < IMAGES.length; i++) {
     let imgSrc = IMAGES[i].getAttribute("src");
-    imgSrc = imgSrc.slice(0,-8);
+    imgSrc = imgSrc.slice(0, -8);
     let srcset = makeSrcset(imgSrc);
-    console.log(srcset);
+    IMAGES[i].setAttribute("srcset", srcset);
+    // console.log(srcset);
 
     let type = IMAGES[i].getAttribute("data-type");
     let sizes = SIZES[type];
-    console.log(sizes);
+    IMAGES[i].setAttribute("sizes", sizes);
+    // console.log(sizes);
 }
