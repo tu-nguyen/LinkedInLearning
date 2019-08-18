5584cc41d9da6a5ced415e0530b5ce1d9c8f0550
tu-nguyen <valcrune@gmail.com>
2019-08-17 23:51:13 -0400

    finished ch2, and progress with ch3 of Learning Python

--- Master Python for Data Science/Learning Python/Ch3/dates_start.py
+++ Master Python for Data Science/Learning Python/Ch3/dates_start.py
@@ -1,27 +1,34 @@
 #
 # Example file for working with date information
 #
+from datetime import date
+from datetime import time
+from datetime import datetime
 
 
 def main():
-  ## DATE OBJECTS
-  # Get today's date from the simple today() method from the date class
+    # DATE OBJECTS
+    # Get today's date from the simple today() method from the date class
+    # today = date.today()
+    # print("Today's date is ", today)
+
+    # # print out the date's individual components
+    # print("Date components: ", today.day, today.month, today.year)
+
+    # # retrieve today's weekday (0=Monday, 6=Sunday)
+    # print("Today;s weekday # is: ", today.weekday())
+    # days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+    # print("Which is a: ", days[today.weekday()])
+
+    # DATETIME OBJECTS
+    # Get today's date from the datetime class
+    today = datetime.now()
+    print("The current date and time is ", today)
+
+    # Get the current time
+    t = datetime.time(datetime.now())
+    print(t)
 
 
-  # print out the date's individual components
-
-  
-  # retrieve today's weekday (0=Monday, 6=Sunday)
-
-  
-  ## DATETIME OBJECTS
-  # Get today's date from the datetime class
-
-
-  # Get the current time
-
-
-  
 if __name__ == "__main__":
-  main();
-  
+    main()
