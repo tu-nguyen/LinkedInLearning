30bf3b397404dee2963b0527e80fee7eaaf5b8c4
tu-nguyen <valcrune@gmail.com>
2019-08-18 18:30:35 -0400

    progress with ch5 of Learning Python

--- Master Python for Data Science/Learning Python/Ch5/jsondata_start.py
+++ Master Python for Data Science/Learning Python/Ch5/jsondata_start.py
@@ -1,38 +1,57 @@
-# 
+#
 # Example file for parsing and processing JSON
 #
-import urllib.request 
+import urllib.request
+import json
 
 
 def printResults(data):
-  # Use the json module to load the string data into a dictionary
-  theJSON = json.loads(data)
-  
-  # now we can access the contents of the JSON like any other Python object
+    # Use the json module to load the string data into a dictionary
+    theJSON = json.loads(data)
 
-  
-  # output the number of events, plus the magnitude and each event name  
+    # now we can access the contents of the JSON like any other Python object
+    if "title" in theJSON["metadata"]:
+        print(theJSON["metadata"]["title"])
+
+    # output the number of events, plus the magnitude and each event name
+    count = theJSON["metadata"]["count"]
+    print(str(count) + " event recorded")
+
+    # for each event, print the place where it occurred
+    for i in theJSON["features"]:
+        print(i["properties"]["place"])
+
+    # print the events that only have a magnitude greater than 4
+    for i in theJSON["features"]:
+        if i["properties"]["mag"] >= 4.0:
+            print("%2.1f" % i["properties"]["mag"], i["properties"]["place"])
+    print("-----------------\n")
+
+    # print only the events where at least 1 person reported feeling something
+    print("Events that were felt:")
+    for i in theJSON["features"]:
+        feltReports = i["properties"]["felt"]
+        if feltReports != None:
+            if feltReports > 0:
+                print("%2.1f" % i["properties"]["mag"], i["properties"]["place"],
+                      " reported " + str(feltReports) + " times")
 
 
-  # for each event, print the place where it occurred
-
-
-  # print the events that only have a magnitude greater than 4
-
-      
-  # print only the events where at least 1 person reported feeling something
-
-  
 def main():
-  # define a variable to hold the source URL
-  # In this case we'll use the free data feed from the USGS
-  # This feed lists all earthquakes for the last day larger than Mag 2.5
-  urlData = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson"
+    # define a variable to hold the source URL
+    # In this case we'll use the free data feed from the USGS
+    # This feed lists all earthquakes for the last day larger than Mag 2.5
+    urlData = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson"
 
-  # Open the URL and read the data
-  webUrl = urllib.request.urlopen(urlData)
-  print ("result code: " + str(webUrl.getcode()))
+    # Open the URL and read the data
+    webUrl = urllib.request.urlopen(urlData)
+    print("result code: " + str(webUrl.getcode()))
+    if(webUrl.getcode() == 200):
+        data = webUrl.read()
+        printResults(data)
+    else:
+        print("Recieved error, cannot parse results")
 
 
 if __name__ == "__main__":
-  main()
+    main()
