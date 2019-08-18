cb316904fde22aa8b0fe7f59c25f2a19184141ca
tu-nguyen <valcrune@gmail.com>
2019-08-18 18:07:37 -0400

    finished ch4 of Learning Python

--- Master Python for Data Science/Learning Python/Ch4/shell_start.py
+++ Master Python for Data Science/Learning Python/Ch4/shell_start.py
@@ -4,28 +4,35 @@
 import os
 from os import path
 import shutil
+from shutil import make_archive
+from zipfile import ZipFile
 
 
 def main():
-  # make a duplicate of an existing file
-  if path.exists("textfile.txt"):
-    # get the path to the file in the current directory
+    # make a duplicate of an existing file
+    if path.exists("textfile.txt"):
+        # get the path to the file in the current directory
+        src = path.realpath("textfile.txt")
 
-    
-    # let's make a backup copy by appending "bak" to the name
+        # let's make a backup copy by appending "bak" to the name
+        dst = src + ".bak"
 
-    
-    # copy over the permissions, modification times, and other info
+        # copy over the permissions, modification times, and other info
+        # shutil.copy(src, dst)
+        # shutil.copystat(src, dst)
+
+        # rename the original file
+        # os.rename("textfile.txt", "newfile.txt")
+
+        # now put things into a ZIP archive
+        # root_dir, tail = path.split(src)
+        # shutil.make_archive("archieve", "zip", root_dir)
+
+        # more fine-grained control over ZIP files
+        with ZipFile("testzip.zip", "w") as newzip:
+            newzip.write("textfile.txt")
+            newzip.write("textfile.txt.bak")
 
 
-    # rename the original file
-
-    
-    # now put things into a ZIP archive
-
-
-    # more fine-grained control over ZIP files
-
-      
 if __name__ == "__main__":
-  main()
+    main()
