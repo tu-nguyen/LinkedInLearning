49a35dc413f8901a942a6e1d0278ca6228ce1a8e
Tu Nguyen <valcrune@gmail.com>
2019-08-21 13:11:33 -0400

    finished Learning Python

--- Master Python for Data Science/Learning Python/Ch5/xmlparsing_start.py
+++ Master Python for Data Science/Learning Python/Ch5/xmlparsing_start.py
@@ -13,8 +13,20 @@ def main():
     print(doc.firstChild.tagName)
 
     # get a list of XML tags from the document and print each one
+    skills = doc.getElementsByTagName("skill")
+    print("%d skills: " % skills.length)
+    for skill in skills:
+        print(skill.getAttribute("name"))
 
     # create a new XML tag and add it into the document
+    newSkill = doc.createElement("skill")
+    newSkill.setAttribute("name", "jQuery")
+    doc.firstChild.appendChild(newSkill)
+
+    skills = doc.getElementsByTagName("skill")
+    print("%d skills: " % skills.length)
+    for skill in skills:
+        print(skill.getAttribute("name"))
 
 
 if __name__ == "__main__":
