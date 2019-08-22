71584d03cdf4decc76e5c9a59d231cbc698a0efa
tu-nguyen <valcrune@gmail.com>
2019-08-21 21:45:19 -0400

    finished ch3 of Python Essential

--- Master Python for Data Science/Python Essential Training 1/Chap03/sequence.py
+++ Master Python for Data Science/Python Essential Training 1/Chap03/sequence.py
@@ -1,6 +1,34 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-x = [ 1, 2, 3, 4, 5 ]
-for i in x:
-    print('i is {}'.format(i))
+# x = [1, 2, 3, 4, 5] <-- list
+# x = {1, 2, 3, 4, 5}  # <-- tuple, not mutable
+# x[2] = 42
+
+# x = list(range(5))
+# x[2] = 42
+# for i in x:
+#     print('i is {}'.format(i))
+
+# x = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5}
+# x["three"] = 42
+# for k, v in x.items():
+#     print('k: {}, v: {}'.format(k, v))
+
+x = (1, "two", 3.0, [4, "four"], 5)
+y = [1, "two", 3.0, [4, "four"], 5]
+print('x is {}'.format(x))
+print(id(x[0]))
+print(id(y[0]))
+
+# if x is y:
+#     print("yup")
+# else:
+#     print("nop")
+
+if isinstance(y, tuple):
+    print("tuple")
+elif isinstance(y, list):
+    print("list")
+else:
+    print("nop")
--- Master Python for Data Science/Python Essential Training 1/Chap03/types.py
+++ Master Python for Data Science/Python Essential Training 1/Chap03/types.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-x = 7
+from decimal import *
+
+# a = 8
+# b = 9
+
+# a = Decimal('.10')
+# b = Decimal(".30")
+# x = .1 + .1 + .1 - .3 returns 5.551115123125793e-17
+# x = a + a + a - b returns correct value of 0.00
+x = 1
 print('x is {}'.format(x))
 print(type(x))
+
+if x:
+    print("True")
+else:
+    print("False")
