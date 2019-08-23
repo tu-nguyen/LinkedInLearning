59b10afc1bdaf666699bdc4da26ef84e3419beca
Tu Nguyen <valcrune@gmail.com>
2019-08-23 12:54:14 -0400

    finished ch4 of Learning Python

--- Master Python for Data Science/Python Essential Training 1/Chap04/conditional.py
+++ Master Python for Data Science/Python Essential Training 1/Chap04/conditional.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-if True:
+x = 42
+
+if x == 0:
     print('if true')
-elif False:
-    print('elif true')
+elif x == 1:
+    print('elif1 true')
+elif x == 2:
+    print('elif2 true')
+elif x == 3:
+    print('elif3 true')
+elif x == 4:
+    print('elif4 true')
+elif x == 5:
+    print('elif5 true')
 else:
     print('neither true')
