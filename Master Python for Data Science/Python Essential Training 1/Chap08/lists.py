5df01234d7b97c9d1c802db458c0b73f29de95f2
Tu Nguyen <valcrune@gmail.com>
2019-08-27 13:06:20 -0400

    progress with ch8 of Python Essential

--- Master Python for Data Science/Python Essential Training 1/Chap08/lists.py
+++ Master Python for Data Science/Python Essential Training 1/Chap08/lists.py
@@ -3,6 +3,15 @@
 
 def main():
     game = [ 'Rock', 'Paper', 'Scissors', 'Lizard', 'Spock' ]
+    # i = game.index("Paper")
+    # print(game[i])
+    # game.append("Computer") # appends to end of list 
+    # game.insert(0, "Computer") # insert at index 0
+    # game.remove("Paper") # removes item by value
+    # game.pop() # pop last item and returns it
+    # del game[3] # removes by index [1:3] by slice as well
+    # print(", ".join(game)) # prints each with a ", "
+    # print(len(game)) # function that returns length
     print_list(game)
 
 def print_list(o):
