b5dbe939113245281489f632f41f34bf14f0ffd5
tu-nguyen <valcrune@gmail.com>
2019-08-29 19:34:57 -0400

    finished ch10 of Python Essential

--- Master Python for Data Science/Python Essential Training 1/Chap10/exceptions.py
+++ Master Python for Data Science/Python Essential Training 1/Chap10/exceptions.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+
 def inclusive_range(*args):
     numargs = len(args)
     start = 0
     step = 1
-    
+
     # initialize parameters
     if numargs < 1:
         raise TypeError(f'expected at least 1 argument, got {numargs}')
@@ -15,7 +16,8 @@ def inclusive_range(*args):
         (start, stop) = args
     elif numargs == 3:
         (start, stop, step) = args
-    else: raise TypeError(f'expected at most 3 arguments, got {numargs}')
+    else:
+        raise TypeError(f'expected at most 3 arguments, got {numargs}')
 
     # generator
     i = start
@@ -23,9 +25,15 @@ def inclusive_range(*args):
         yield i
         i += step
 
-def main():
-    for i in inclusive_range(25):
-        print(i, end = ' ', flush = True)
-    print()
 
-if __name__ == '__main__': main()
+def main():
+    try:
+        for i in inclusive_range(25, 5, 5, 5):
+            print(i, end=' ', flush=True)
+        print()
+    except TypeError as e:
+        print(f"range error: {e}")
+
+
+if __name__ == '__main__':
+    main()
--- Master Python for Data Science/Python Essential Training 1/Chap10/hello.py
+++ Master Python for Data Science/Python Essential Training 1/Chap10/hello.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-def main():
-    print('Hello, World.')
+import sys
 
-if __name__ == '__main__': main()
+
+def main():
+    # print('Hello, World.')
+    try:
+        # x = int("foo")
+        x = 5 / 0
+    except ValueError:  # executes if try fails
+        print("I caught a ValueError")
+        # captures the error and code will continue
+    # ValueError: invalid literal for int() with base 10: 'foo'
+    # except ZeroDivisionError:
+    #     print("don't divide by zero")
+    except:
+        print(f"unknown error: {sys.exc_info()}")
+    else:  # only executes if you don't have an error
+        print("good job!")
+
+
+if __name__ == '__main__':
+    main()
