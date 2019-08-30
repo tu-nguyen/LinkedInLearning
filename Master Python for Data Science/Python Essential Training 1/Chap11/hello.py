aee3df232da2eefe1796919595f7e5e039f9be42
Tu Nguyen <valcrune@gmail.com>
2019-08-30 12:28:15 -0400

    progress with ch11 for Python Essential

--- Master Python for Data Science/Python Essential Training 1/Chap11/hello.py
+++ Master Python for Data Science/Python Essential Training 1/Chap11/hello.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-print('Hello, World.')
+# print('Hello, World.'.swapcase())
+# print('Hello, World. {}'.format(42 * 7))
+# print("""
+#     Hello, 
+#     World.
+
+#     {}
+#     """.format(42 * 7))
+# s = 'Hello, World. {}'
+# print(s.format(42 * 7))
+
+class MyString(str):
+    def __str__(self):
+        return self[::-1]
+
+s = MyString('Hello, World.')
+print(s)
