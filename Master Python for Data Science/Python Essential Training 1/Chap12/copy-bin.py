d4c3c9167f26095bde9302e9570ade98dc3fbdd0
tu-nguyen <valcrune@gmail.com>
2019-09-02 11:15:54 -0400

    finished ch12 of Python Essential

--- Master Python for Data Science/Python Essential Training 1/Chap12/copy-bin.py
+++ Master Python for Data Science/Python Essential Training 1/Chap12/copy-bin.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+
 def main():
-    infile = open('berlin.jpg', 'rb')
+    infile = open('berlin.jpg', 'rb')  # rt would return an error
     outfile = open('berlin-copy.jpg', 'wb')
     while True:
         buf = infile.read(10240)
         if buf:
             outfile.write(buf)
             print('.', end='', flush=True)
-        else: break
+        else:
+            break
     outfile.close()
     print('\ndone.')
 
-if __name__ == '__main__': main()
+
+if __name__ == '__main__':
+    main()
--- Master Python for Data Science/Python Essential Training 1/Chap12/files.py
+++ Master Python for Data Science/Python Essential Training 1/Chap12/files.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+
 def main():
-    f = open('lines.txt')
+    f = open('lines.txt')  # default is r for read
+    # other option is w, which clears the file, a which appends to end of file
     for line in f:
         print(line.rstrip())
 
-if __name__ == '__main__': main()
+
+if __name__ == '__main__':
+    main()
