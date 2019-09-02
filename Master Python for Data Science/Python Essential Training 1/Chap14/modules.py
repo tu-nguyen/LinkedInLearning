4c8c94dd7cd528f3724bfd5652647c7c9740ebf1
tu-nguyen <valcrune@gmail.com>
2019-09-02 17:05:08 -0400

    finished ch14 of Python Essential

--- Master Python for Data Science/Python Essential Training 1/Chap14/modules.py
+++ Master Python for Data Science/Python Essential Training 1/Chap14/modules.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-import sys
+# import sys
+# import os
+# import random
+import datetime
+
 
 def main():
-    v = sys.version_info
-    print('Python version {}.{}.{}'.format(*v))
+    # v = sys.version_info
+    # print('Python version {}.{}.{}'.format(*v))
+    # v = sys.platform
+    # print(v)
+    # v = os.name
+    # print(v)
+    # v = os.getcwd()
+    # print(v)
+    # x = random.randint(1, 1000)
+    # print(x)
+    # x = list(range(25))
+    # print(x)
+    # random.shuffle(x)
+    # print(x)
+    now = datetime.datetime.now()
+    print(now.year)
 
-if __name__ == '__main__': main()
+
+if __name__ == '__main__':
+    main()
