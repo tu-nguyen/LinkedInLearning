e5032ff277a1006d188a17c305b199db888ec35e
tu-nguyen <valcrune@gmail.com>
2019-09-02 16:05:29 -0400

    progress with ch 13 of Python Essential

--- Master Python for Data Science/Python Essential Training 1/Chap13/types.py
+++ Master Python for Data Science/Python Essential Training 1/Chap13/types.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-x = '47'
-y = int(x)
+x = 47
+y = int(x)  # constructor for the int type
+# y = float(x)  # constructor for the float type
+# y = abs(x)  # constructor for the int type but returns abs value
+# y = divmod(x, 3) # returns the quotient and remainder
+# y = complex(x, 73)  # imaginary numbers # same as = 47 + 73j
 
 print(f'x is {type(x)}')
 print(f'x is {x}')
