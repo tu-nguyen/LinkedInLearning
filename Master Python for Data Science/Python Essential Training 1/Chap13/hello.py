664bb68e07b83d60cae8819f66f894c26c6fddd1
tu-nguyen <valcrune@gmail.com>
2019-09-02 16:44:05 -0400

    finished ch13 of Python Essential

--- Master Python for Data Science/Python Essential Training 1/Chap13/hello.py
+++ Master Python for Data Science/Python Essential Training 1/Chap13/hello.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-print('Hello, World.')
+
+# class bunny:
+#     def __init__(self, n):
+#         self._n = n
+
+#     def __repr__(self):
+#         return f'repr: the number of bunnies is {self._n}'
+
+#     def __str__(self):
+#         return f'str: the number of bunnies is {self._n}'
+
+
+# s = 'Hello, World.'
+# x = bunny(47)
+# print(ascii(x))  # best possible screen representation
+# print(chr(128406))
+
+# x = (1, 2, 3, 4, 5)
+# # y = sum(x, 10)
+# y = max(x)
+# print(x)
+# print(y)
+
+x = 42
+y = isinstance(x, int)
+
+print(x)
+print(y)
