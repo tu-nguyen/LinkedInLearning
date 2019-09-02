91dd11834fcb1f6823603af1c7de9318dbc8494c
tu-nguyen <valcrune@gmail.com>
2019-09-02 10:44:30 -0400

    finished ch11 of Python Essential

--- Master Python for Data Science/Python Essential Training 1/Chap11/hello.py
+++ Master Python for Data Science/Python Essential Training 1/Chap11/hello.py
@@ -4,7 +4,7 @@
 # print('Hello, World.'.swapcase())
 # print('Hello, World. {}'.format(42 * 7))
 # print("""
-#     Hello, 
+#     Hello,
 #     World.
 
 #     {}
@@ -12,9 +12,12 @@
 # s = 'Hello, World. {}'
 # print(s.format(42 * 7))
 
-class MyString(str):
-    def __str__(self):
-        return self[::-1]
+# class MyString(str):
+#     def __str__(self):
+#         return self[::-1]
 
-s = MyString('Hello, World.')
-print(s)
+# s = MyString('Hello, World.')
+# print(s)
+x = 42
+# y = 73
+print(f"The number is {x:.3f}")  # .format(x))
--- Master Python for Data Science/Python Essential Training 1/Chap11/split-join.py
+++ Master Python for Data Science/Python Essential Training 1/Chap11/split-join.py
@@ -2,4 +2,8 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 s = 'This is a long string with a bunch of words in it.'
-print(s)
+# print(s.split()) # splits the string into a list
+# print(s.split('i'))  # splits on the letter i
+l = s.split()
+s2 = ":".join(l)  # returns This:is:a:long:string:with:a:bunch:of:words:in:it.
+print(s2)
